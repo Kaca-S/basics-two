1ae0966b31ff5d7ec8a45a9ce41b6790f5aca499
Kaca-S <karolinasosic@gmail.com>
2025-04-06 20:34:55 +0200

    modified main.js

--- main.js
+++ main.js
@@ -2,21 +2,20 @@ const userAccount = {
     accountName: "Karol",
     balance: 100,
 
-    getBalance() {
+getBalance() {
         alert(`The total balance for ${this.accountName} is $${this.balance}`);
     },
-
- deposit() {
-        let moneyIn = parseFloat(prompt("Enter the amount of money you want to deposit:"));
-        if (!isNaN(moneyIn) && moneyIn > 0) {
-            let newBalance = this.balance += moneyIn;
-            this.balance = newBalance;
-            alert(`Deposit successful! Your new balance is $${this.balance}`);
-        } else {
-            this.accountError("Please enter a valid positive number.");
-        }
-    },
-  withdrawal() {
+deposit() {
+    let moneyIn = parseFloat(prompt("Enter the amount of money you want to deposit:"));
+    if (!isNaN(moneyIn) && moneyIn > 0) {
+        let newBalance = this.balance += moneyIn;
+        this.balance = newBalance;
+        alert(`Deposit successful! Your new balance is $${this.balance}`);
+    } else {
+        this.accountError("Please enter a valid positive number.");
+    }
+},
+withdrawal() {
     let moneyOut = parseFloat(prompt("Enter the amount of money you want to take out:"));
     if (!isNaN(moneyOut) && moneyOut >= 0) {
         if (moneyOut <= this.balance) {
@@ -29,19 +28,19 @@ const userAccount = {
         this.accountError("Please enter a valid positive number.");
         }
     },
-
-    getAccountName() {
+getAccountName() {
     alert(`Account holder's name is ${this.accountName}`);
     return this.accountName;
     },
     accountError(message) {
         alert(`Account Error: ${message}`);
     }
-  };
+};
 
-  function exitAccount() {
+function exitAccount() {
     alert(`Exiting the account. Thank you for using the ATM!`);
 }
+
 /*while loop (Transaction) to allow the user multiple transactions until they choose to exit.
 The loop lasts as long as Transaction is true. false when the user selects 5.) exit.*/
 
